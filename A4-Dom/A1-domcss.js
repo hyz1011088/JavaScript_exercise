92bc19cf31bd3add7d1fb188e8dabc9be0ba4932
hyz1011088 <hyz1011088@gmail.com>
2016-05-27 13:00:16 +0800

    dom

---
+++ A4-Dom/A1-domcss.js
@@ -0,0 +1,1 @@
+<!DOCTYPE html><html><body><p id="p1">Hello World!</p><p id="p2">Hello World!</p><script>document.getElementById("p2").style.color="blue";document.getElementById("p2").style.fontFamily="Arial";document.getElementById("p2").style.fontSize="larger";</script><p>以上段落通过脚本修改。</p></body></html>
