5facab2d5dc8800af567ceec220612c2a790d20d
hyz1011088 <hyz1011088@gmail.com>
2017-01-21 18:27:54 +0800

    array

---
+++ A4-Object/5-array/array1 - concat - 副本 (2).js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>²ËÄñ½Ì³Ì(runoob.com)</title>
+</head>
+<body>
+	
+<script>
+var hege = ["Cecilie", "Lone"];
+var stale = ["Emil", "Tobias", "Linus"];
+var kai = ["Robin"];
+var children = hege.concat(stale,kai);
+document.write(children);
+</script>
+	
+</body>
+</html>
