8051dc59f100af765c4dda0caa18b236ee0fbf0d
hyz1011088 <hyz1011088@gmail.com>
2017-01-22 17:18:56 +0800

    math

---
+++ A4-Object/8-regexp/regexp - i.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>²ËÄñ½Ì³Ì(runoob.com)</title>
+</head>
+<body>
+
+<script>
+var str = "Visit RUnoob";
+var patt1 = /runoob/i;
+document.write(str.match(patt1));
+</script>
+
+</body>
+</html>
